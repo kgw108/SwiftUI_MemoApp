6ce99c3cfbd942ba0ba2ecfb46a876e930f1c2e6
kgw108 <kkw0098@naver.com>
2022-08-17 21:49:01 +0900

    Step1

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 강고운 on 2022/08/17.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
