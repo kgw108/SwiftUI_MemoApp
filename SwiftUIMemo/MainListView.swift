b70d3ce0d09734e1ed836c6f2a0cac3c93f155f4
kgw108 <kkw0098@naver.com>
2022-08-17 22:34:27 +0900

    Step3

--- SwiftUIMemo/MainListView.swift
+++ SwiftUIMemo/MainListView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct MainListView: View {
+    @EnvironmentObject var store: MemoStore
+    //공유데이터 목록 확인 후 동일한 타입의 인스턴스 존재할 경우, 속성을 해당 인스턴스로 자동 초기화
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(store.list) { memo in
+                VStack(alignment: .leading) {
+                    Text(memo.content)
+                        .font(.body)
+                        .lineLimit(1) // 텍스트 최대 1줄만 나오게
+                    
+                    Text(memo.insertData, style: .date)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .listStyle(.plain)
+            .navigationTitle("내 메모") // NavigationView에 모디파이어(메소드) 추가
+        }
     }
 }
 
 struct MainListView_Previews: PreviewProvider {
     static var previews: some View {
         MainListView()
+            .environmentObject(MemoStore())//프리뷰에서 사용할 커스텀 공유 데이터 따로 등록!
     }
 }
--- SwiftUIMemo/SwiftUIMemoApp.swift
+++ SwiftUIMemo/SwiftUIMemoApp.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 @main
 struct SwiftUIMemoApp: App {
+    @StateObject var store = MemoStore() //메모 저장소 속성으로 저장
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             MainListView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environmentObject(store) // MainListView와 이어지는 뷰에서 동일한 객체 쉽게 사용 가능
         }
     }
 } 
